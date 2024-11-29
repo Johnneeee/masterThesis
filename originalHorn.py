901efb2d1995a097662a37a79abccb1e0c5dade5
Johnny Nguyen <jng006@uib.no>
2024-11-29 19:46:33 +0100

    restoring original horn algorithm

--- originalHorn.py
+++ originalHorn.py
@@ -28,11 +28,11 @@ def evalFormula(formula, x, V):
     """
         eg:
             input = (
-                    ~(v15 & v19),
-                    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
-                    [v0, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29],
-                )
-            output = True if formula holds according to x, else False
+                    Implies(v0, v1),
+                    [1, 1, 0, 0],
+                    [v0, v1, v2, v3],
+                    )
+            output = True (True if formula holds according to the example x, else False )
     """
     if formula == true:
         return True
@@ -42,18 +42,20 @@ def evalFormula(formula, x, V):
     for i in range(len(V)):
         a[V[i]] = (True if x[i] == 1
                         else False)
+
     return True if formula.subs(a) == True else False
 
 def set2theory(set):
     """
-        sorting (weird kind of sorting) and transforms set to theory
+        sorting (weird kind of sorting) and formatting set to theory
         eg:
-            input = {~(v1 & v2), ~(v0 & v2)}
-            output = ~(v0 & v2) & ~(v1 & v2)
+            input = {Implies(v1 & v2, v3), Implies(v0, v1)}
+            output = (Implies(v0, v1)) & (Implies(v1 & v2, v3))
     """ 
     tempt = True
     for e in set:
         tempt = tempt & e
+        
     return tempt
 
 def entails(T,clause,V):
@@ -128,7 +130,7 @@ def learn(V,iterations=-1):
             for s in S:
                 s_intersection_x = [1 if s[index] ==1 and counterExample[index] == 1 else 0 for index in range(len(V))]
                 A = {index for index,value in enumerate(s_intersection_x) if value == 1}
-                B = {index for index,value in enumerate(s) if value ==1 }
+                B = {index for index,value in enumerate(s) if value == 1 }
                 if A.issubset(B) and not B.issubset(A): # A properly contained in B
                     idx = S.index(s)
                     if MQ(s_intersection_x, V, T) == False and s_intersection_x:
@@ -148,9 +150,13 @@ def learn(V,iterations=-1):
 
 
 V = define_variables(4) # our vocabulary V
+
+#Generating random target
 T = generate_target(V, 2) # the target concept T ### we dont know this in practice
-# print(T)
-# Target = {Implies(v1 & v2, v0), Implies(v0 & v1, v3)}
+
+# Setting known target
+# T = {Implies(V[0], V[1]), Implies(V[1] & V[2], V[3])}
+
 finalH = learn(V, iterations=10)
 
 print("")
