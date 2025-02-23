c67377be87e8f8983e96233fa40ba0b1dcd38e73
Johnny Nguyen <jng006@uib.no>
2025-02-23 14:52:16 +0100

    calculates total time here, in seconds and in normal

--- probeRule/analyzeMetadata.py
+++ probeRule/analyzeMetadata.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 
 #getting available filenames
 lms = ["xlmRBase", "xlmRLarge", "mBertUncased", "mBertCased", "nbBertBase", "nbBertLarge", "norbert", "norbert2"]
@@ -9,19 +10,41 @@ for lm in lms:
     for tag in tags:
         allFiles.append(f"{lm}_{tag}")
 
+
+#####################################################################################################################
+def addTotalRuntime(file):
+    newData = []
+    with open(f"output_data/metadataRaw/{file}.csv", mode = "r",encoding="UTF-8") as f:
+        totalRt = 0
+        csvFile = csv.reader(f, delimiter=";")
+        next(csvFile)
+        for line in csvFile:
+            totalRt = round(totalRt + float(line[-1]),3)
+            newline = line + [totalRt] + [str(datetime.timedelta(seconds=int(totalRt)))]
+            newData.append(newline)
+
+    with open(f"output_data/metadataTotalRuntime/{file}.csv", 'w', newline='',encoding="UTF-8") as csvfile:
+        writer = csv.writer(csvfile, delimiter=";")
+        writer.writerow(["ITERATION","LEN(HYP)","SAMPLENR","RUNTIME(sample)","RUNTIME(total)s","RUNTIME(total)"])
+        writer.writerows(newData)
+
+for file in allFiles:
+    addTotalRuntime(file)
+
+addTotalRuntime("ahahhahahah_i10_r0")
 #####################################################################################################################
 #gather final runtime to one file
 def gatherMetadata(writeTo, files):
     metadataTotal = []
 
     for file in files:
-        with open(f"output_data/metadata/{file}.csv", mode = "r",encoding="UTF-8") as f:
+        with open(f"output_data/metadataRaw/{file}.csv", mode = "r",encoding="UTF-8") as f:
             runtime = f.readlines()[-1].split(";")[-1][:-1] # runtime total
             metadataTotal.append([file,runtime])
 
-    with open(f"output_data/metadataTotal/{writeTo}.csv", 'w', newline='',encoding="UTF-8") as csvfile:
+    with open(f"output_data/metadataAll/{writeTo}.csv", 'w', newline='',encoding="UTF-8") as csvfile:
         writer = csv.writer(csvfile, delimiter=";")
         writer.writerow(["RUN","RUNTIME"])
         writer.writerows(metadataTotal)
 
-# gatherMetadata("allRuntimes(total)", allFiles)
+gatherMetadata("allRuntimes(all)", allFiles)
