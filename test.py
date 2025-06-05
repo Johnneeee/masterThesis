2261f4598b2de91f0119fa6a76234bd373a9aae9
Johnny Nguyen <jng006@uib.no>
2025-06-05 21:27:28 +0200

    sorting files

--- test.py
+++ test.py
@@ -1,38 +1,5 @@
-#   Introduction
-#       Motivation
-# 	    Research Questions
-# 	    Thesis Outline
-#   Background
-# 	    Language models
-#           Transformers
-#           Bert architecture
-# 	    Biases in language models
-#           The template-based approach
-#           Some of the discovered biases
-# 	    Extracting horn rules via queries
-#           Angluin's exact learning framework
-#           The exact horn algorithm
-#           Applying Horn algorithm on langauge models
-#   Preparing census data
-# 	    Ssb.no
-# 	    Utdanning.no
-#   Probing language models
-# 	    Probing gender given occupation
-# 		    Method
-# 		    Results
-# 	    Probing occupation given name
-# 		    Method
-# 		    Results
-# 	    Template sentences results comparison
-# 		    Results
-# 	    Extracting horn rules
-# 		    Input choises
-# 		    Method
-# 		    Results
-# 			    Rules
-# 			    Metadata 
-#   Related works
-#   Discussion
-#       Conclusion
-#       Limitations
-#       Future work
+import matplotlib.pyplot as plt  
+    
+plt.title("$x^{my text}$")
+plt.plot([1, 2, 3, 4], [16, 4, 1, 8]) 
+plt.show()
